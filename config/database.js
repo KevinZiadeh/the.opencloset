750dc61c92b625381c618d9037c336f8a58bb594
Kevin Ziadeh <kevinziadeh@gmail.com>
2020-11-19 13:00:39 +0200

    Removed config files

--- config/database.js
+++
@@ -1,6 +0,0 @@
-let Admin = require('./admin');
-
-module.exports = {
-  database:process.env.MONGODB_URI || 'mongodb://'+Admin.mlabuser+':'+Admin.mlabpass+'@ds247310.mlab.com:47310/theopencloset',
-  secret:'mysecret'
-};
